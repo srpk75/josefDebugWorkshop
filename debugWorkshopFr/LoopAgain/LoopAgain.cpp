36217f6f0403a8346a0de906d6ce3f7b2263c41a
srpk75 <avodaalgamal@gmail.com>
2024-12-15 19:00:14 +0200

    fixed first issue

--- debugWorkshopFr/LoopAgain/LoopAgain.cpp
+++ debugWorkshopFr/LoopAgain/LoopAgain.cpp
@@ -1,8 +1,20 @@
+/*
+Debug docs:
+purpose: code asks user for a size, and prints the fibonachi sequence i think?
+		 i think thats what its doing
+execution: loop is infinite and its printing a bunch of numbers
+
+- because size is unsigned it overflows to the highest positive value allowed
+	[fix] change size to a signed int and yes it was fibonachi sequence im the goat
+
+*/
+
+
 #include <iostream>
 
 int main()
 {
-	unsigned int size = 0;
+	int size = 0;
 
 	int t1 = 0, t2 = 1;
 
